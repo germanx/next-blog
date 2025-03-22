fdd7275a3c0e9c26384d4a80e37a59278c279efa
germanx <german.khokhlov@gmail.com>
2025-03-22 10:01:41 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nextjs Blog
+
+## json server
+
+npm i --save-dev json-server@0.17.4
---
+++ app/components/Card.js
@@ -0,0 +1,9 @@
+function Card({ children, className }) {
+  return (
+    <div className={`border rounded-md border-gray-600 p-4 ${className}`}>
+      {children}
+    </div>
+  );
+}
+
+export default Card;
